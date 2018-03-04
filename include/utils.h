c15d24ee80daeb94a24192eb741c115e63b9879c
tuz358 <orca00358@gmail.com>
2018-03-04 11:34:01 +0900

    first commit

---
+++ include/utils.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+
+uint32_t swap_endian32(uint32_t data);
+
---
+++ utils.cpp
@@ -0,0 +1,13 @@
+#include "include/utils.h"
+
+uint32_t swap_endian32(uint32_t data){
+  uint32_t swapped = 0x00;
+
+  swapped |=  data               << 24;
+  swapped |= (data & 0x0000ff00) <<  8;
+  swapped |= (data & 0x00ff0000) <<  8;
+  swapped |=  data               >> 24;
+
+  return swapped;
+}
+
