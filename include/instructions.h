9c186fd2474ea569e1f36ef71724ff6f007a9a4d
tuz358 <orca00358@gmail.com>
2018-03-06 11:19:45 +0900

    Add sub_eax_imm32(0x2d) instruction

--- include/instructions.h
+++ include/instructions.h
@@ -38,6 +38,7 @@ public:
   void and_eax_imm32(); // 0x25
   void sub_rm32_r32();  // 0x29
   void sub_r32_rm32();  // 0x2b
+  void sub_eax_imm32(); // 0x2d
   void xor_rm32_r32();  // 0x31
   void inc_eax();       // 0x40
   void inc_ecx();       // 0x41
