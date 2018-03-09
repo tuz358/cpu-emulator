394e9820155a70c5794bc669c71f036dd5a47134
tuz358 <orca00358@gmail.com>
2018-03-09 11:21:43 +0900

    Add comments

--- sample/fibonacci.asm
+++ sample/fibonacci.asm
@@ -4,8 +4,11 @@
 ; NASM version 2.11.08
 ; $ nasm fibonacci.asm -o fibonacci.bin
 ; $ hexdump -C fibonacci.bin
-;
-;
+; 00000000  66 31 db 66 b9 09 00 00  00 66 31 d2 66 b8 01 00  |f1.f.....f1.f...|
+; 00000010  00 00 eb 00 66 89 da 66  89 c3 66 01 d0 66 49 66  |....f..f..f..fIf|
+; 00000020  83 f9 00 75 ef f4                                 |...u..|
+; 00000026
+; $
 
 section .text
 global _start
